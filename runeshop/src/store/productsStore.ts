48a1bc29af6c992af61ab17246e62101423b598f
Rodrigo Romero <rodri_08romero@hotmail.com>
2025-05-14 21:03:58 -0300

    Avances en store

---
+++ runeshop/src/store/productsStore.ts
@@ -0,0 +1,31 @@
+import { create } from 'zustand';
+import type { IProducto } from '../types/IProducto';
+
+interface IProductsStore {
+    products: IProducto[];
+    setArrayProducts: (products: IProducto[]) => void;
+    activeProduct: IProducto | null;
+    setActiveProduct: (product: IProducto | null) => void;
+    addProduct: (product: IProducto) => void;
+    updateProduct: (product: IProducto) => void;
+    deleteProduct: (id: number) => void;
+}
+
+export const productsStore = create<IProductsStore>((set) => ({
+    products: [],
+    setArrayProducts: (productsArray) => set(() => ({ products: productsArray })),
+    activeProduct: null,
+    setActiveProduct: (activeProductIn) => set(() => ({ activeProduct: activeProductIn })),
+    addProduct: (newproduct) => set((state) => ({ products: [...state.products, newproduct] })),
+    updateProduct: (updatedProduct) =>
+		set((state) => {
+			const updatedProducts = state.products.map((productA) =>
+				productA.id === updatedProduct.id ? { ...productA, ...updatedProduct } : productA
+			);
+			return { products: updatedProducts };
+		}),
+    deleteProduct: (productId) => set((state) => {
+        const updatedProducts = state.products.filter((p) => p.id !== productId);
+        return { products: updatedProducts };
+    }),
+}));
