311e5ba4afe7f46f9646ae4a4634677048974ec3
Merce <mercetetilla@gmail.com>
2025-05-16 22:50:53 -0300

    UserProfile avanzado

--- runeshop/src/components/Screens/UserProfileScreen/UserProfileScreen.tsx
+++ runeshop/src/components/Screens/UserProfileScreen/UserProfileScreen.tsx
@@ -1,21 +1,16 @@
+import { Footer } from "../../ui/Footer/Footer";
+import { Header } from "../../ui/Header/Header";
+import { UserProfile } from "../../ui/UserProfile/UserProfile";
 import styles from "./UserProfileScreen.module.css";
 
 export const UserProfileScreen = () => {
-	return (
-		<div className={styles.principalContainerUserProfile}>
-            <h1>Mi perfil</h1>
-			<div className={styles.dataContainerUserProfile}>
-                <div className={styles.userDataForm}>
-                <span className="material-symbols-outlined">account_circle</span>
-                <div>
-                    <input type="text" name="user" placeholder="Nombre de usuario" />
-                    <input type="text" name="name" placeholder="Nombre" />
-                </div>
-                </div>
-                <div className={styles.userAdressesForm}>
-
-                </div>
-            </div>
-		</div>
-	);
+  return (
+    <div className={styles.containerUserProfileScreen}>
+      <Header />
+      <div className={styles.containerUserProfileScreenContent}>
+        <UserProfile />
+        <Footer />
+      </div>
+    </div>
+  );
 };
---
+++ runeshop/src/components/ui/UserProfile/UserProfile.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import styles from './UserProfile.module.css'
+
+export const UserProfile = () => {
+  return (
+    
+      <div className={styles.principalContainerUserProfile}>
+        <div className={styles.dataContainerUserProfile}>
+          <div className={styles.userDataForm}>
+            <div className={styles.userIconForm}>
+              <span className="material-symbols-outlined">account_circle</span>
+            </div>
+            <div className={styles.userDataFormInputs}>
+              <input type="text" name="user" placeholder="Nombre de usuario" />
+              <input type="text" name="name" placeholder="Nombre" />
+              <input type="text" name="lastName" placeholder="Apellido" />
+              <input type="text" name="mail" placeholder="Correo electrónico" />
+              <input type="number" name="dni" placeholder="DNI" />
+            </div>
+          </div>
+          <div className={styles.userAdressesForm}>
+            <h2>Direcciones registradas</h2>
+            <div className={styles.userAdressesInformation}>
+              <div className={styles.userAdresses}>
+                <p>1FALTA condicional de vacias Dirección de ejemplo</p>
+                <div className={styles.userAdressesButtons}>
+                  <button className={styles.userAdressesEditButton}>
+                    <span className="material-symbols-outlined">edit</span>
+                  </button>
+                  <button className={styles.userAdressesDeleteButton}>
+                    <span className="material-symbols-outlined">delete</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className={styles.userAdressesAddButton}>
+              <button> Agregar dirección</button>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
