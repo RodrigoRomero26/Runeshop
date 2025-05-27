b290bef6d63ae533112747aa5f4deee5f9dacacb
Merce <mercetetilla@gmail.com>
2025-05-27 16:41:31 -0300

    avances productos

--- runeshop/src/components/ui/ProductScreenComponents/ProductScreenComponents.tsx
+++ runeshop/src/components/ui/ProductScreenComponents/ProductScreenComponents.tsx
@@ -9,50 +9,82 @@ export const ProductScreenComponents = () => {
         <div className={styles.productInfoContainer}>
           <div className={styles.productImagesPerspectiveContainer}>
             <img
-              src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
+              src="https://th.bing.com/th/id/OIP.YZxt1qIMGtTRsEVzIiOkfQHaHa?rs=1&pid=ImgDetMain"
               alt="Product"
-              className={styles.productImage}
+              className={styles.productImagePerspective}
             />
             <img
               src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
               alt="Product"
-              className={styles.productImage}
+              className={styles.productImagePerspective}
+            />
+            <img
+              src="https://th.bing.com/th/id/OIP.YZxt1qIMGtTRsEVzIiOkfQHaHa?rs=1&pid=ImgDetMain"
+              alt="Product"
+              className={styles.productImagePerspective}
+            />
+            <img
+              src="https://th.bing.com/th/id/OIP.7yXv7DXhB_SVtqnlOcxqFAHaH6?rs=1&pid=ImgDetMain"
+              alt="Product"
+              className={styles.productImagePerspective}
             />
             <img
               src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
               alt="Product"
-              className={styles.productImage}
+              className={styles.productImagePerspective}
             />
             <img
               src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
               alt="Product"
-              className={styles.productImage}
+              className={styles.productImagePerspective}
             />
           </div>
           <img
-            src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
+            src="https://th.bing.com/th/id/OIP.YZxt1qIMGtTRsEVzIiOkfQHaHa?rs=1&pid=ImgDetMain"
             alt="Product"
             className={styles.productPrincipalImage}
           />
           <div className={styles.productDetailsContainer}>
             <h2 className={styles.productTitle}>Product Title</h2>
             <p className={styles.productPrice}>$2000000</p>
-            <p className={styles.productPromos}>Promos</p>
+            <p className={styles.productPromos}>
+              Hasta 5 x $40.000 sin interes
+            </p>
             <div className={styles.productColors}>
               <img
                 src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
                 alt="Product"
-                className={styles.productImage}
+                className={styles.productImageColor}
+              />
+              <img
+                src="https://th.bing.com/th/id/OIP.7yXv7DXhB_SVtqnlOcxqFAHaH6?rs=1&pid=ImgDetMain"
+                alt="Product"
+                className={styles.productImageColor}
+              />
+              <img
+                src="https://th.bing.com/th/id/OIP.YZxt1qIMGtTRsEVzIiOkfQHaHa?rs=1&pid=ImgDetMain"
+                alt="Product"
+                className={styles.productImageColor}
               />
               <img
                 src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
                 alt="Product"
-                className={styles.productImage}
+                className={styles.productImageColor}
+              />
+              <img
+                src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
+                alt="Product"
+                className={styles.productImageColor}
+              />
+              <img
+                src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
+                alt="Product"
+                className={styles.productImageColor}
               />
             </div>
-            <div className={styles.productSizes}>
-              <h3 className={styles.productSizesTitle}>Talles</h3>
-              <div className={styles.sizeOptionsCheckboxes}>
+            <div className={styles.sizesContainer}>
+              <h3>Talles</h3>
+              <div className={styles.checkboxSizeContainer}>
                 {[33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46].map(
                   (size) => (
                     <label key={size} className={styles.sizeLabel}>
@@ -67,36 +99,53 @@ export const ProductScreenComponents = () => {
                 )}
               </div>
             </div>
-            <button className={styles.addToCartButton}>Añadir al carrito</button>
+            <button className={styles.addToCartButton}>
+              Añadir al carrito
+            </button>
           </div>
         </div>
         <div className={styles.similarProductsContainer}>
           <h2 className={styles.similarProductsTitle}>Productos similares</h2>
           <div className={styles.similarProductsImagesContainer}>
             <img
-              src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
+              src="https://th.bing.com/th/id/OIP.7yXv7DXhB_SVtqnlOcxqFAHaH6?rs=1&pid=ImgDetMain"
               alt="Product"
-              className={styles.productImage}
+              className={styles.similarProductImage}
+              onClick={() => {
+                console.log("Imagen clickeada 1");
+              }}
+            />
+            <img
+              src="https://th.bing.com/th/id/OIP.YZxt1qIMGtTRsEVzIiOkfQHaHa?rs=1&pid=ImgDetMain"
+              alt="Product"
+              className={styles.similarProductImage}
+              onClick={() => {
+                console.log("Imagen clickeada 2");
+              }}
+            />
+            <img
+              src="https://th.bing.com/th/id/OIP.YZxt1qIMGtTRsEVzIiOkfQHaHa?rs=1&pid=ImgDetMain"
+              alt="Product"
+              className={styles.similarProductImage}
+              onClick={() => {
+                console.log("Imagen clickeada 3");
+              }}
             />
             <img
               src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
               alt="Product"
-              className={styles.productImage}
+              className={styles.similarProductImage}
+              onClick={() => {
+                console.log("Imagen clickeada 4");
+              }}
             />
             <img
-              src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
+              src="https://th.bing.com/th/id/OIP.7yXv7DXhB_SVtqnlOcxqFAHaH6?rs=1&pid=ImgDetMain"
               alt="Product"
-              className={styles.productImage}
-            />
-            <img
-              src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
-              alt="Product"
-              className={styles.productImage}
-            />
-            <img
-              src="https://imgs.search.brave.com/1EuIsDeRffUn4TbvODgTEofZPv4x2vot022dkxpGAbQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kYXNo/LnZ0ZXhhc3NldHMu/Y29tL2FycXVpdm9z/L2lkcy8xNTE4NTM1/LTUwMC1hdXRvP3Y9/NjM4NjI5NjAwNjg4/OTMwMDAwJndpZHRo/PTUwMCZoZWlnaHQ9/YXV0byZhc3BlY3Q9/dHJ1ZQ"
-              alt="Product"
-              className={styles.productImage}
+              className={styles.similarProductImage}
+              onClick={() => {
+                console.log("Imagen clickeada 5");
+              }}
             />
           </div>
         </div>
