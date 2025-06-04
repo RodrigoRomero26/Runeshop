ca12b11db92b0427f79c640cfd3f272a5cb8a4c3
Merce <mercetetilla@gmail.com>
2025-06-04 16:32:45 -0300

    avances responsive

--- runeshop/src/components/ui/Aside/Aside.tsx
+++ runeshop/src/components/ui/Aside/Aside.tsx
@@ -22,115 +22,114 @@ export const Aside = () => {
   return (
     //SE ABRE APRETANDO EL BOTON DE HOMBRES
     <>
-    <div
-          className={styles.menuIconMobile}
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span className="material-symbols-outlined">
-filter_alt
-</span>
-        </div>
-    <div className={styles.principalContainerAside}>
-      <div className={styles.filtersContainer}>
-        <h2>Filtros de busqueda</h2>
-        <button className={styles.buttonFilter}>Aplicar filtros</button>
-        <button className={styles.buttonFilter}>Limpiar filtros</button>
-        <div className={styles.filterContainer}>
-          <h3>Genero</h3>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Mujer" />
-            Mujer
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Hombre" />
-            Hombre
-          </label>
-        </div>
-        <div className={styles.filterContainer}>
-          <h3>Tipo de indumentaria</h3>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Remera" />
-            Remera
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Pantalon" />
-            Pantalon
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Calzado" />
-            Calzado
-          </label>
-        </div>
-        <div className={styles.filterContainer}>
-          <h3>Categorias</h3>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Running" />
-            Running
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Urbano" />
-            Urbano
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Casual" />
-            Casual
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Trail" />
-            Trail
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Entrenamiento" />
-            Entrenamiento
-          </label>
-        </div>
-        <div className={styles.filterContainer}>
-          <h3>Marca</h3>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Nike" />
-            Nike
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Adidas" />
-            Adidas
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Puma" />
-            Puma
-          </label>
-          <label className={styles.checkboxLabel}>
-            <input type="checkbox" name="Reebok" />
-            Reebok
-          </label>
-        </div>
-        <div className={styles.filterContainer}>
-          <h3>Talles</h3>
-          <div className={styles.checkboxSizeContainer}>
-            {[33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46].map(
-              (size) => (
-                <label key={size} className={styles.sizeLabel}>
-                  <input
-                    type="checkbox"
-                    name={String(size)}
-                    className={styles.hiddenCheckbox}
-                  />
-                  <span>{size}</span>
-                </label>
-              )
-            )}
+      <div
+        className={styles.menuIconMobile}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span className="material-symbols-outlined">filter_alt</span>
+      </div>
+      <div className={styles.principalContainerAside}>
+        <div className={styles.filtersContainer}>
+          <h2>Filtros de busqueda</h2>
+          <button className={styles.buttonFilter}>Aplicar filtros</button>
+          <button className={styles.buttonFilter}>Limpiar filtros</button>
+          <div className={styles.filterContainer}>
+            <h3>Genero</h3>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Mujer" />
+              Mujer
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Hombre" />
+              Hombre
+            </label>
+          </div>
+          <div className={styles.filterContainer}>
+            <h3>Tipo de indumentaria</h3>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Remera" />
+              Remera
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Pantalon" />
+              Pantalon
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Calzado" />
+              Calzado
+            </label>
+          </div>
+          <div className={styles.filterContainer}>
+            <h3>Categorias</h3>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Running" />
+              Running
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Urbano" />
+              Urbano
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Casual" />
+              Casual
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Trail" />
+              Trail
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Entrenamiento" />
+              Entrenamiento
+            </label>
+          </div>
+          <div className={styles.filterContainer}>
+            <h3>Marca</h3>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Nike" />
+              Nike
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Adidas" />
+              Adidas
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Puma" />
+              Puma
+            </label>
+            <label className={styles.checkboxLabel}>
+              <input type="checkbox" name="Reebok" />
+              Reebok
+            </label>
+          </div>
+          <div className={styles.filterContainer}>
+            <h3>Talles</h3>
+            <div className={styles.checkboxSizeContainer}>
+              {[33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46].map(
+                (size) => (
+                  <label key={size} className={styles.sizeLabel}>
+                    <input
+                      type="checkbox"
+                      name={String(size)}
+                      className={styles.hiddenCheckbox}
+                    />
+                    <span>{size}</span>
+                  </label>
+                )
+              )}
+            </div>
+          </div>
+          <div className={styles.filterContainer}>
+            <span className="material-symbols-outlined">paid</span>
+            <label className={styles.priceLabel}>
+              <input type="text" placeholder="Maximo" />
+            </label>
+            <label className={styles.priceLabel}>
+              <input type="text" placeholder="Minimo" />
+            </label>
           </div>
         </div>
-        <div className={styles.filterContainer}>
-          <span className="material-symbols-outlined">paid</span>
-          <label className={styles.priceLabel}>
-            <input type="text" placeholder="Maximo"/>
-          </label>
-          <label className={styles.priceLabel}>
-            <input type="text" placeholder="Minimo"/>
-          </label>
-        </div>
       </div>
-       {menuOpen && (
+      {menuOpen && (
         <div className={styles.mobileMenu} ref={menuRef}>
           <h2>Filtros de búsqueda</h2>
           <button className={styles.buttonFilter}>Aplicar filtros</button>
@@ -211,16 +210,18 @@ filter_alt
           <div className={styles.filterContainer}>
             <h3>Talles</h3>
             <div className={styles.checkboxSizeContainer}>
-              {[33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46].map((size) => (
-                <label key={size} className={styles.sizeLabel}>
-                  <input
-                    type="checkbox"
-                    name={String(size)}
-                    className={styles.hiddenCheckbox}
-                  />
-                  <span>{size}</span>
-                </label>
-              ))}
+              {[33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46].map(
+                (size) => (
+                  <label key={size} className={styles.sizeLabel}>
+                    <input
+                      type="checkbox"
+                      name={String(size)}
+                      className={styles.hiddenCheckbox}
+                    />
+                    <span>{size}</span>
+                  </label>
+                )
+              )}
             </div>
           </div>
 
@@ -235,7 +236,6 @@ filter_alt
           </div>
         </div>
       )}
-    </div>
     </>
   );
 };
